4dea5f96aff2d6f78cb140bf08c1588d4aa34480
Jose Pablo <artaviajosepablo687@gmail.com>
2025-03-19 15:57:07 -0600

    start with goals section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { Banner } from './components/Banner'
 import { Navbar } from './components/Navbar'
-
+import { GoalsSection } from './components/GoalsSection'
 import './App.css'
 
 export function App() {
@@ -13,6 +13,7 @@ export function App() {
    <div class="xl-64">
       <Navbar />
       <Banner />
+      <GoalsSection />
     </div>
   </>
   )
