c1c0647909e249d0a109d40021aaae647279da5f
Jose Pablo <artaviajosepablo687@gmail.com>
2025-03-25 10:44:03 -0600

    working on goalsSection

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,7 +2,7 @@ import {Check} from '../assets/check'
 export function Card(props){
     return(
         
-        <div class="m-10 flex group md:flex-row relative w-50 h-70 overflow-hidden rounded-[16px] bg-gray-300 p-[1px] transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500">
+        <div class="my-5 md:my-10 flex group border border-blue-800 md:flex-row relative w-70 h-{60px} overflow-hidden rounded-[16px] bg-gray-300 p-[1px] transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500">
             <div class="group-hover:animate-spin-slow invisible absolute -top-40 -bottom-40 left-10 right-10 bg-gradient-to-r from-transparent via-white/90 to-transparent group-hover:visible"></div>
             <div class="h-full relative rounded-[15px] bg-purple p-6">
                 <Check />
@@ -11,8 +11,7 @@ export function Card(props){
                 <p class="text-lg font-semibold text-purple-500">{props.name}</p>
                 <p class="font-md text-slate-500">{props.text}</p>
             </div>
-            
-        </div>
+            </div>
         </div>
     )
 }
--- frontend/src/components/GoalsSection.jsx
+++ frontend/src/components/GoalsSection.jsx
@@ -2,11 +2,10 @@ import { Card } from './Card'
 
 export function GoalsSection (){
     return(
-        
-            <div className='mt-20 mb-30 flex flex-row w-2/3  flex-wrap justify-center'>
-                <Card name = 'Prueba' text = 'Hola esto es una prueba de texto'/>
-                <Card name = 'Value' text = 'Hola esto es una prueba de texto'/>
-                <Card name = 'Test' text = 'Hola esto es una prueba de texto' />
+            <div className='mt-20 mb-30 flex flex-row md:w-2/3 w-4/5  flex-wrap md:justify-between justify-center'>
+                <Card name = 'AMPLIAR PRESENCIA EN EL MERCADO' text = 'Ampliar la presencia en el mercado mediante la apertura de nuevas sucursales físicas y una plataforma de venta en línea para llegar a más clientes a nivel nacional e internacional.'/>
+                <Card name = 'PROMOVER EL TALENTO LOCAL' text = 'Colaborando con artistas emergentes y consagrados, ofreciendo sus obras en nuestra galería y organizando exposiciones exclusivas'/>
+                <Card name = 'FOMENTAR LA PERSONALIZACION' text = 'Permitiendo a los clientes encargar cuadros a medida, adaptados a sus gustos y necesidades específicas, garantizando una experiencia única y satisfactoria.' />
             </div>
       
     )
