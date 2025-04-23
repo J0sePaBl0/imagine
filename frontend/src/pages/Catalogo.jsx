bf82ab426e59751ed65866b0d867ccfde7838a12
Jose Pablo <artaviajosepablo687@gmail.com>
2025-04-23 16:16:29 -0600

    start with catalog

--- frontend/src/pages/Catalogo.jsx
+++ frontend/src/pages/Catalogo.jsx
@@ -1,7 +1,14 @@
 import { Layout } from '../layouts/Layout'
+import { Card } from '../components/Card'
 export function Catalogo (){
     
     return(
-        <h1>Hola</h1>
+        <div className='border border-amber-500 w-full h-96 flex flex-row'>
+            <div className='border border-blue-200 w-1/4 h-full'>
+            </div>
+            <div className='border border-red-500 h-full w-3/4 pl-10'>
+            
+            </div>
+        </div>
     )
 }
