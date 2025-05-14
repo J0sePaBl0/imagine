42cf8e3a89f3a967b2039939a803333b62fdd751
Jose Pablo <artaviajosepablo687@gmail.com>
2025-05-14 10:51:53 -0600

    creating login and signin pages and linking with navbar buttons

--- frontend/src/layouts/Navbar.jsx
+++ frontend/src/layouts/Navbar.jsx
@@ -106,7 +106,7 @@ export function Navbar() {
                     >
                       <div className="py-1">
                         <a
-                          href="/registro"
+                          href="/signin"
                           className="block px-4 py-2 text-sm text-gray-300 hover:bg-gray-700 hover:text-white"
                         >
                           Registrarse
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { Catalogo } from '../pages/Catalogo'
+import { SignIn } from '../pages/SignIn'
+import { LogIn } from '../pages/LogIn'
 
 const AppRoutes = () => {
 
@@ -8,6 +10,8 @@ const AppRoutes = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/login" element={<LogIn/>} />
+          <Route path="/signin" element={<SignIn/>} />
         </Routes>
       )
     }
