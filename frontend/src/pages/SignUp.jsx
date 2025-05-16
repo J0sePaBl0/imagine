9d3a55f22f7cd890973285c40048e487d0f92806
Jose Pablo <artaviajosepablo687@gmail.com>
2025-05-16 15:25:25 -0600

    fixing files

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export function SignIn() {
+export function SignUp() {
   const [formData, setFormData] = useState({
     username: '',
     email: '',
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { Catalogo } from '../pages/Catalogo'
-import { SignIn } from '../pages/SignIn'
+import { SignUp } from '../pages/SignUp'
 import { LogIn } from '../pages/LogIn'
 
 const AppRoutes = () => {
@@ -11,7 +11,7 @@ const AppRoutes = () => {
           <Route path="/" element={<Home />} />
           <Route path="/catalogo" element={<Catalogo />} />
           <Route path="/login" element={<LogIn/>} />
-          <Route path="/signin" element={<SignIn/>} />
+          <Route path="/signin" element={<SignUp/>} />
         </Routes>
       )
     }
