85fdc3c717487c20de68966fb182a376bd1312a6
Jose Pablo <artaviajosepablo687@gmail.com>
2025-07-02 11:06:42 -0600

    fixing cors errors 3

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const paintRoutes = require('./src/routes/paintRoutes');
+const userRoutes = require('./src/routes/userRoutes');
+
 const app = express();
 
-const whitelist = process.env.FRONTEND_URL.split(',');
+app.use(cors());
+app.use(express.json());
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-};
 
-app.use(cors(corsOptions));
-app.use(express.json()); // ← importante si estás usando POST con JSON
+app.use('/api/paints', paintRoutes);
+app.use('/api/users', userRoutes);
 
-// Rutas
-try {
-  const paintRoutes = require('./src/routes/paintRoutes');
-  const userRoutes = require('./src/routes/userRoutes');
-  app.use('/api/paints', paintRoutes);
-  app.use('/api/users', userRoutes);
-} catch (err) {
-  console.error('ROUTE LOADING ERROR:', err);
-  process.exit(1);
+
+if (require.main === module) {
+
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => {
+    console.log(`Server running locally on http://localhost:${PORT}`);
+  });
+} else {
+
+  module.exports = app;
 }
-
-// Error handler para CORS
-app.use((err, req, res, next) => {
-  if (err.message === 'Not allowed by CORS') {
-    return res.status(403).json({ error: 'CORS error: Origin not allowed' });
-  }
-  next(err);
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
